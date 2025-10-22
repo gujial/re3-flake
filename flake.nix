3c44437c3096cb033e832c4b74e7c3a0f7903fff
gujial <gu18647403665@outlook.com>
2025-10-22 10:28:32 +0800

    修正 desktop 文件的图标路径和可执行文件路径以匹配不同的分支名称。

--- flake.nix
+++ flake.nix
@@ -21,7 +21,12 @@
             owner = "gujial";
             repo = "re3-flake";
             rev = "refs/heads/${branch}";
-            sha256 = "WCvs5QGkfnj33yu26LdSVTtwhuLyB3NhkT1i1nirvCk=";
+            sha256 =
+              if branch == "master" then "WCvs5QGkfnj33yu26LdSVTtwhuLyB3NhkT1i1nirvCk="
+              else if branch == "miami" then "VlDLAptJ4eRzyjtTfHwsvqAvHsRDbYfadPV1uR8nkZU="
+              else if branch == "lcs" then "s3GLZcggNiv0yBFe5qflwcGU7RSVOwsogW2v2/s7nZ0="
+              else
+                throw "Unknown branch: ${branch}";
             fetchSubmodules = true;
           };
 
@@ -52,18 +57,25 @@
             make config=$config
           '';
 
+          program = 
+            if branch == "master" then "re3"
+            else if branch == "miami" then "reVC"
+            else if branch == "lcs" then "reLCS"
+            else
+              throw "Unknown branch: ${branch}";
+
           installPhase = ''
                       mkdir -p $out/bin
-                      cp ../bin/linux-amd64-librw_gl3_glfw-oal/Release/re3 $out/bin/re3-${branch}
-                      cp ../res/images/logo.svg $out/bin/re3-${branch}-icon.svg
+                      cp ../bin/linux-amd64-librw_gl3_glfw-oal/Release/${program} $out/bin/${program}
+                      cp ../res/images/logo.svg $out/bin/${program}.svg
 
-                      cat > $out/bin/re3-${branch}.desktop <<EOF
+                      cat > $out/bin/${program}.desktop <<EOF
             [Desktop Entry]
-            Name=Re3 (${branch})
-            Exec=$out/bin/re3-${branch}
-            Icon=re3-${branch}-icon
+            Name=${program}
+            Exec=$out/bin/${program}
+            Icon=$out/bin/${program}.svg
             Type=Application
-            Categories=Game;Emulator;
+            Categories=Game;
             Terminal=false
             EOF
           '';
@@ -83,7 +95,7 @@
     in
     {
       packages.x86_64-linux = {
-        re3-master = buildRe3 "master" "Re3 master branch: the mainline GTA III engine port.";
+        re3 = buildRe3 "master" "Re3 master branch: the mainline GTA III engine port.";
         re3-vc = buildRe3 "miami" "Re3 Miami (VC) branch: GTA Vice City engine port.";
         re3-lcs = buildRe3 "lcs" "Re3 LCS branch: GTA Liberty City Stories engine port.";
       };
